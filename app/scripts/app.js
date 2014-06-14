9c6726b05b24224e418e0d85ea449553f65dc343
joverall22 <joverall22@gmail.com>
2014-06-14 17:52:36 -0600

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+﻿var app = new angular.module('app', []);
+
+app.controller('linksCtrl', ['$scope', function ($scope) {
+    $scope.internalLinks = [{
+        'name': 'magazine',
+        'link': 'magazine',
+        'class': 'test'
+    }, {
+        'name': 'about',
+        'link': 'about',
+        'class': 'test'
+    }, {
+        'name': 'sign up',
+        'link': 'sign-up',
+        'class': 'test'
+    }, {
+        'name': 'log in',
+        'link': 'log-in',
+        'class': 'test'
+    }
+    ];
+    $scope.externalLinks = [{
+        'name': '@svbtle',
+        'link': 'twitter.com/svbtle',
+        'class': 'test'
+    }
+    ];
+}])
