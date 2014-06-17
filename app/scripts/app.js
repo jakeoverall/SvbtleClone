a25e145a931c78bfaa97be209a3343167683a72a
joverall22 <joverall22@gmail.com>
2014-06-17 02:20:28 -0600

    adds media folder

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,26 +3,53 @@
 app.controller('linksCtrl', ['$scope', function ($scope) {
     $scope.internalLinks = [{
         'name': 'magazine',
-        'link': 'magazine',
-        'class': 'test'
+        'link': 'magazine'
     }, {
         'name': 'about',
-        'link': 'about',
-        'class': 'test'
+        'link': 'about'
     }, {
         'name': 'sign up',
-        'link': 'sign-up',
-        'class': 'test'
+        'link': 'sign-up'
     }, {
         'name': 'log in',
-        'link': 'log-in',
-        'class': 'test'
+        'link': 'log-in'
     }
     ];
     $scope.externalLinks = [{
         'name': '@svbtle',
-        'link': 'twitter.com/svbtle',
-        'class': 'test'
+        'link': 'twitter.com/svbtle'
     }
     ];
-}])
+}]);
+
+app.controller('articlesCtrl', ['$scope', function ($scope) {
+    $scope.articles = {
+        'url': '../views/articles/articles.html'
+    };
+}]);
+
+$(function () {
+    var menu = $('#menu');
+    var navBar = $('#rightNavBar');
+    var menuButton = $('#rightNavMenuButton');
+    var buttonIcon = $('#navMenuIcon');
+    var menuOpen = false;
+    var navText = $("#navText");
+    menuButton.on('mousedown', function (e) {
+        if (!menuOpen) {
+            e.preventDefault();
+            menuOpen = true;
+            navText.addClass('hidden');
+            navBar.css({ 'width': '150px' });
+            buttonIcon.addClass('glyphicon-remove').removeClass('glyphicon-chevron-down');
+            menu.removeClass('hidden');
+        } else {
+            e.preventDefault();
+            menuOpen = false;
+            navBar.css({ 'width': '55px' });
+            navText.removeClass('hidden');
+            buttonIcon.addClass('glyphicon-chevron-down').removeClass('glyphicon-remove');
+            menu.addClass('hidden');
+        }
+    });
+});
